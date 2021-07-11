5b758d73a382e87165c69c084e68e8225008f7bd
kowshikvenkat <79137094+kowshikvenkat@users.noreply.github.com>
2021-07-11 20:26:22 +0530

    Add files via upload

---
+++ VS Code/script.js
@@ -0,0 +1,12 @@
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("button3");
+var span = document.getElementsByClassName("close")[0];
+
+ function myFunction() {
+  modal.style.display = "block";
+}
+ function myFunction1() {
+  modal.style.display = "none";
+}
+
---
+++ VS Code/script2.js
@@ -0,0 +1,11 @@
+
+var modal_1 = document.getElementById("myModals");
+var btn_1 = document.getElementById("button4");
+var span_1 = document.getElementsByClassName("closes")[0];
+
+ function myFunction2() {
+  modal.style.display = "block";
+}
+ function myFunction3() {
+  modal.style.display = "none";
+}
